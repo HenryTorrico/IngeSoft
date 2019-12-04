87f09f645350e77cbf44bb41fffc3ecc78d6e2ab
Javier Loayza Flores <javo_tralll@hotmail.com>
2019-12-04 18:04:51 -0400

    agregando movimiento al auto y pruebas

--- lib/auto.rb
+++ lib/auto.rb
@@ -1,37 +1,124 @@
 class Auto
 
-    posicion_x=0
-    posicion_y=0
+   # def initialize()
+    #    @posicion_x = 0
+    #    @posicion_y = 0
+    #    @direccion = 'N'
+    #    @position = [@posicion_x,@posicion_y,@direccion]
+    #end
 
-    direccion='N'
+    def initialize(posx,posy,dir)
+        @posicion_x = posx
+        @posicion_y = posy
+        @direccion = dir
+        @position = [@posicion_x,@posicion_y,@direccion]
+    end
 
+    def getPosition()
+        return @position
+    end
 
+    def updatePosition()
+        @position = [@posicion_x,@posicion_y,@direccion]
+    end
+    
     def setx(num)
         if(num>0)
-            posicion_x=num
-            return posicion_x
+            @posicion_x=num
+            updatePosition()
         else
-            return nil
+            puts "valor no valido"
         end
     end
     
+    def getx()
+        return @posicion_x
+    end
 
     def sety(num)
         if(num>0)
-            posicion_y=num
-            return posicion_y
+            @posicion_y=num
+            updatePosition()
         else
-            return nil
+            puts "valor no valido"
         end
+    end
+
+    def gety()
+        return @posicion_y
     end
 
     def setdir (dir)
         if(dir=='N'||dir=='S'||dir=='E'||dir=='W')
-            direccion=dir
-            return direccion
+            @direccion=dir
+            updatePosition()
         else
-            return nil
+            puts "valor no valido"
         end
     end
-    
+
+    def getdir ()
+        return @direccion
+    end
+
+    def mover (mov)
+        if(mov=='A')
+            if(@direccion=='N')
+                @posicion_y = @posicion_y + 1
+            end
+            if(@direccion=='E')
+                @posicion_x = @posicion_x + 1
+            end
+            if(@direccion=='W')
+                @posicion_x = @posicion_x - 1
+            end
+            if(@direccion=='S')
+                @posicion_y = @posicion_y - 1
+            end
+        end
+        if(mov=='D')
+            if(@direccion=='N')
+                @posicion_x = @posicion_x + 1
+                @direccion='E'
+            else
+                if(@direccion=='E')
+                    @posicion_y = @posicion_y - 1
+                    @direccion='S'
+                else
+                    if(@direccion=='W')
+                        @posicion_y = @posicion_y + 1
+                        @direccion='N'
+                    else
+                        if(@direccion=='S')
+                            @posicion_x = @posicion_x - 1
+                            @direccion='W'
+                        end
+                    end
+                end
+            end
+        end
+        if(mov=='I')
+            if(@direccion=='N')
+                @posicion_x = @posicion_x - 1
+                @direccion='W'
+            else
+                if(@direccion=='E')
+                    @posicion_y = @posicion_y + 1
+                    @direccion='N'
+                else
+                    if(@direccion=='W')
+                        @posicion_y = @posicion_y - 1
+                        @direccion='S'
+                    else
+                        if(@direccion=='S')
+                            @posicion_x = @posicion_x + 1
+                            @direccion='E'
+                        end
+                    end
+                end
+            end
+        end
+        updatePosition()
+    end
+
 end
--- spec/auto_spec.rb
+++ spec/auto_spec.rb
@@ -1,50 +1,161 @@
 require 'auto'
 RSpec.describe Auto do
-    #controlando funcion set
 
+    #controlando funcion set y get
     it 'deberia devolver 1 para 1 en x' do
-        auto= Auto.new
-        expect(auto.setx(1)).to eq(1)
-    end
-
-    it 'deberia devolver error para numeros no positivos en x' do
-        auto= Auto.new
-        expect(auto.setx(0)).to eq(nil)
+        auto = Auto.new(0, 0, 'N')
+        auto.setx(1)
+        expect(auto.getx()).to eq(1)
     end
 
     it 'deberia devolver 1 para 1 en y' do
-        auto= Auto.new
-        expect(auto.sety(1)).to eq(1)
-    end
-
-    it 'deberia devolver error para numeros no positivos en y' do
-        auto= Auto.new
-        expect(auto.sety(0)).to eq(nil)
+        auto= Auto.new(0, 0, 'N')
+        auto.sety(1)
+        expect(auto.gety()).to eq(1)
     end
 
     it 'deberia devolver direccion N para N' do
-        auto= Auto.new
-        expect(auto.setdir('N')).to eq('N')
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('N')
+        expect(auto.getdir()).to eq('N')
     end
 
     it 'deberia devolver direccion S para S' do
-        auto= Auto.new
-        expect(auto.setdir('S')).to eq('S')
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('S')
+        expect(auto.getdir()).to eq('S')
     end
 
     it 'deberia devolver direccion E para E' do
-        auto= Auto.new
-        expect(auto.setdir('E')).to eq('E')
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('E')
+        expect(auto.getdir()).to eq('E')
     end
 
     it 'deberia devolver direccion W para W' do
-        auto= Auto.new
-        expect(auto.setdir('W')).to eq('W')
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('W')
+        expect(auto.getdir()).to eq('W')
     end
 
-    it 'deberia devolver error para direccion invalida' do
-        auto= Auto.new
-        expect(auto.setdir('A')).to eq(nil)
+    it 'deberia devolver la posicion actual' do
+        auto= Auto.new(0, 0, 'N')
+        expect(auto.getPosition()).to eq([0,0,'N'])
+    end
+
+    it 'deberia devolver la posicion actual despues de cambios' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setx(5)
+        auto.sety(7)
+        auto.setdir('S')
+        expect(auto.getPosition()).to eq([5,7,'S'])
+    end
+
+    #movimiento adelante
+    it 'deberia aumentar en Y al avanzar hacia el norte' do
+        auto= Auto.new(0, 0, 'N')
+        y=auto.gety()
+        auto.setdir('N')
+        auto.mover('A')
+        expect(auto.gety()).to eq(y+1)
+    end
+
+    it 'deberia aumentar en X al avanzar hacia el este' do
+        auto= Auto.new(0, 0, 'N')
+        x=auto.getx()
+        auto.setdir('E')
+        auto.mover('A')
+        expect(auto.getx()).to eq(x+1)
+    end
+
+    it 'deberia disminuir en y al avanzar hacia el sud' do
+        auto= Auto.new(0, 0, 'N')
+        y=auto.gety()
+        auto.setdir('S')
+        auto.mover('A')
+        expect(auto.gety()).to eq(y-1)
+    end
+
+    it 'deberia disminuir en X al avanzar hacia el oeste' do
+        auto= Auto.new(0, 0, 'N')
+        x=auto.getx()
+        auto.setdir('W')
+        auto.mover('A')
+        expect(auto.getx()).to eq(x-1)
+    end
+
+    
+    it 'deberia cambiar direccion al este si gira a la derecha desde el norte y aumentar en x' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('N')
+        x=auto.getx()
+        auto.mover('D')
+        expect(auto.getdir()).to eq('E')
+        expect(auto.getx()).to eq(x+1)
+    end
+
+    it 'deberia cambiar direccion al sud si gira a la derecha desde el este y disminuir en y' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('E')
+        y=auto.gety()
+        auto.mover('D')
+        expect(auto.getdir()).to eq('S')
+        expect(auto.gety()).to eq(y-1)
+    end
+
+    it 'deberia cambiar direccion al oeste si gira a la derecha desde el sud y disminuir en x' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('S')
+        x=auto.getx()
+        auto.mover('D')
+        expect(auto.getdir()).to eq('W')
+        expect(auto.getx()).to eq(x-1)
+    end
+
+    it 'deberia cambiar direccion al norte si gira a la derecha desde el oeste y aumentar en y' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('W')
+        y=auto.gety()
+        auto.mover('D')
+        expect(auto.getdir()).to eq('N')
+        expect(auto.gety()).to eq(y+1)
+    end
+    
+    #movimiento izquierda
+    it 'deberia cambiar direccion al oeste si gira a la izquierda desde el norte y disminuir en x' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('N')
+        x=auto.getx()
+        auto.mover('I')
+        expect(auto.getdir()).to eq('W')
+        expect(auto.getx()).to eq(x-1)
+    end
+
+    it 'deberia cambiar direccion al norte si gira a la izquierda desde el este y aumentar en y' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('E')
+        y=auto.gety()
+        auto.mover('I')
+        expect(auto.getdir()).to eq('N')
+        expect(auto.gety()).to eq(y+1)
+    end
+
+    it 'deberia cambiar direccion al este si gira a la izquierda desde el sud y aumentar en x' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('S')
+        x=auto.getx()
+        auto.mover('I')
+        expect(auto.getdir()).to eq('E')
+        expect(auto.getx()).to eq(x+1)
+    end
+
+    it 'deberia cambiar direccion al sud si gira a la izquierda desde el oeste y disminuir en y' do
+        auto= Auto.new(0, 0, 'N')
+        auto.setdir('W')
+        y=auto.gety()
+        auto.mover('I')
+        expect(auto.getdir()).to eq('S')
+        expect(auto.gety()).to eq(y-1)
     end
 
 end
