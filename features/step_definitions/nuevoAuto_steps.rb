c3f3b75fab3d10812a00eda73b803f912a69119d
ElYoquer <guiller_bauti@gmail.com>
2019-12-13 02:30:09 -0400

    ultimo

---
+++ features/step_definitions/nuevoAuto_steps.rb
@@ -0,0 +1,10 @@
+#Given("estoy viendo {string}") do |string|
+ #   visit(string)
+ # end
+ # When("presiono el boton en terreno {string}") do |string|
+ #   click_button(string)
+ # end
+ # 
+  #Then("deberia ver el mensaje en autoTerreno {string}") do |string|
+  #  last_response.body.should =~ /#{string}/ms
+  #end
